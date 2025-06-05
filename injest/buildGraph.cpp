725f1416d1d81c2f5588b19f436a96066803af9c
will donnelly <willdonnelly@wills-MacBook-Air.local>
2025-06-05 10:51:14 +0300

    std::move neededNodeIds to handler to reduce memory footprint

--- injest/buildGraph.cpp
+++ injest/buildGraph.cpp
@@ -12,8 +12,9 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <utility>
 
-// 1a) Handler for collecting bike-friendly ways
+// Handler for collecting bike-friendly ways
 struct WayCollector : public osmium::handler::Handler
 {
     std::unordered_map<uint64_t, std::vector<uint64_t>>& wayNodesMap;
@@ -41,6 +42,25 @@ struct WayCollector : public osmium::handler::Handler
     }
 };
 
+// Handler for collecting needed nodes
+struct NodeCollector : public osmium::handler::Handler
+{
+    std::unordered_set<uint64_t> neededNodeIds;
+    std::unordered_map<uint64_t, std::pair<float, float>>& nodeCoords;
+
+    NodeCollector(std::unordered_set<uint64_t> neededNodeIds,
+                std::unordered_map<uint64_t, std::pair<float, float>>& nodeCoords) :
+                neededNodeIds(std::move(neededNodeIds)),
+                nodeCoords(nodeCoords) {}
+
+    void node(const osmium::Node& node) {
+        uint64_t id = node.id();
+        if (neededNodeIds.count(id)) {
+            nodeCoords[id] = {static_cast<float>(node.location().lat()), static_cast<float>(node.location().lon())};
+        }
+    }
+};
+
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
@@ -53,21 +73,32 @@ int main(int argc, char* argv[])
     WayCollector wc(wayNodesMap);
 
     // ─── Pass 1: Collect bike-friendly ways ──────────────────────────────
-    osmium::io::Reader reader(osmFile);
-    osmium::apply(reader, wc);
-    reader.close();
+
+    osmium::io::Reader readerPassOne(osmFile);
+    osmium::apply(readerPassOne, wc);
+    readerPassOne.close();
 
     std::cout << "waynodesmap size: " << wayNodesMap.size() << "\n";
 
+    // ─── Pass 2: Collect node coordinates ────────────────────────────────
+
     // Build neededNodeIds
     std::unordered_set<uint64_t> neededNodeIds;
-
     for (auto& [wayId, nodesList] : wayNodesMap) {
         for (uint64_t nid : nodesList) {
             neededNodeIds.insert(nid);
         }
     }
     std::cout << "Will collect coords for " << neededNodeIds.size() << " nodes.\n";
+
+    std::unordered_map<uint64_t, std::pair<float, float>> nodeCoords;
+    NodeCollector nc(std::move(neededNodeIds), nodeCoords);
+
+    osmium::io::Reader readerPassTwo(osmFile);
+    osmium::apply(readerPassTwo, nc);
+    readerPassTwo.close();
+
+    std::cout << "Collected " << nodeCoords.size() << " node coordinates.\n";
 
     return 0;
 }
